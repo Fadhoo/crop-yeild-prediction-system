d2ee2a7530448af3184490ac47d53183b607acc8
Fadipe Al-Ameen <fadipetomi00@gmail.com>
2021-09-30 07:40:34 -0700

    initial commit

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,253 @@
+<template>
+  <div class="content">
+    <div class="md-layout">
+      <div
+        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
+      >
+        <stats-card data-background-color="green">
+          <template slot="header">
+            <md-icon>store</md-icon>
+          </template>
+
+          <template slot="content">
+            <p class="category">Wind</p>
+            <h3 class="title"> 34km/h</h3>
+          </template>
+
+          <template slot="footer">
+            <div class="stats">
+              <md-icon>date_range</md-icon>
+              Last 24 Hours
+            </div>
+          </template>
+        </stats-card>
+      </div>
+      <div
+        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
+      >
+        <stats-card data-background-color="orange">
+          <template slot="header">
+            <md-icon>content_copy</md-icon>
+          </template>
+
+          <template slot="content">
+            <p class="category">Rainfall</p>
+            <h3 class="title">
+              75
+              <small>%</small>
+            </h3>
+          </template>
+
+          <template slot="footer">
+            <div class="stats">
+              <md-icon class="text-danger">warning</md-icon>
+              <a href="#pablo">Heavy Rain...</a>
+            </div>
+          </template>
+        </stats-card>
+      </div>
+      <div
+        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
+      >
+        <stats-card data-background-color="red">
+          <template slot="header">
+            <md-icon>info_outline</md-icon>
+          </template>
+
+          <template slot="content">
+            <p class="category">Temperature</p>
+            <h3 class="title">25 C</h3>
+          </template>
+
+          <template slot="footer">
+            <div class="stats">
+              <md-icon>Rainfall</md-icon>
+              Probability of rain 70%
+            </div>
+          </template>
+        </stats-card>
+      </div>
+      <div
+        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
+      >
+        <stats-card data-background-color="blue">
+          <template slot="header">
+            <i class="fab fa-twitter"></i>
+          </template>
+
+          <template slot="content">
+            <p class="category">Humidity</p>
+            <h3 class="title">+2</h3>
+          </template>
+
+          <template slot="footer">
+            <div class="stats">
+              <md-icon>update</md-icon>
+              Just Updated
+            </div>
+          </template>
+        </stats-card>
+      </div>
+      <div
+        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
+      >
+        <form>
+          <md-card>
+            <md-card-header data-background-color="green">
+              <h4 class="title">Enter Required Details for Prediction.</h4>
+              <p class="category"></p>
+            </md-card-header>
+
+            <md-card-content>
+              <div class="md-layout">
+                <!-- <div class="md-layout-item md-small-size-100 md-size-33">
+                  <md-field>
+                    <label>Company (disabled)</label>
+                    <md-input v-model="disabled" disabled></md-input>
+                  </md-field>
+                </div> -->
+                <div class="md-layout-item md-small-size-100 md-size-33">
+                  <md-field>
+                    <label>Crop</label>
+                    <md-input v-model="username" type="text"></md-input>
+                  </md-field>
+                </div>
+                <div class="md-layout-item md-small-size-100 md-size-33">
+                  <md-field>
+                    <label>Season</label>
+                    <md-input v-model="emailadress" type="email"></md-input>
+                  </md-field>
+                </div>
+                <div class="md-layout-item md-small-size-100 md-size-50">
+                  <md-field>
+                    <label>State Name</label>
+                    <md-input v-model="firstname" type="text"></md-input>
+                  </md-field>
+                </div>
+                <div class="md-layout-item md-small-size-100 md-size-50">
+                  <md-field>
+                    <label>Local Government Name</label>
+                    <md-input v-model="lastname" type="text"></md-input>
+                  </md-field>
+                </div>
+                
+                <div class="md-layout-item md-size-100 text-right">
+                  <md-button class="md-raised md-success">Generate Prediction</md-button>
+                </div>
+              </div>
+            </md-card-content>
+          </md-card>
+        </form>
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  StatsCard,
+  // ChartCard,
+  // NavTabsCard,
+  // NavTabsTable,
+  // OrderedTable
+} from "@/components";
+
+export default {
+  components: {
+    StatsCard,
+    // ChartCard,
+    // NavTabsCard,
+    // NavTabsTable,
+    // OrderedTable
+  },
+  data() {
+    return {
+      dailySalesChart: {
+        data: {
+          labels: ["M", "T", "W", "T", "F", "S", "S"],
+          series: [[12, 17, 7, 17, 23, 18, 38]]
+        },
+        options: {
+          lineSmooth: this.$Chartist.Interpolation.cardinal({
+            tension: 0
+          }),
+          low: 0,
+          high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+          chartPadding: {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0
+          }
+        }
+      },
+      dataCompletedTasksChart: {
+        data: {
+          labels: ["12am", "3pm", "6pm", "9pm", "12pm", "3am", "6am", "9am"],
+          series: [[230, 750, 450, 300, 280, 240, 200, 190]]
+        },
+
+        options: {
+          lineSmooth: this.$Chartist.Interpolation.cardinal({
+            tension: 0
+          }),
+          low: 0,
+          high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+          chartPadding: {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0
+          }
+        }
+      },
+      emailsSubscriptionChart: {
+        data: {
+          labels: [
+            "Ja",
+            "Fe",
+            "Ma",
+            "Ap",
+            "Mai",
+            "Ju",
+            "Jul",
+            "Au",
+            "Se",
+            "Oc",
+            "No",
+            "De"
+          ],
+          series: [[542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]]
+        },
+        options: {
+          axisX: {
+            showGrid: false
+          },
+          low: 0,
+          high: 1000,
+          chartPadding: {
+            top: 0,
+            right: 5,
+            bottom: 0,
+            left: 0
+          }
+        },
+        responsiveOptions: [
+          [
+            "screen and (max-width: 640px)",
+            {
+              seriesBarDistance: 5,
+              axisX: {
+                labelInterpolationFnc: function(value) {
+                  return value[0];
+                }
+              }
+            }
+          ]
+        ]
+      }
+    };
+  }
+};
+</script>
